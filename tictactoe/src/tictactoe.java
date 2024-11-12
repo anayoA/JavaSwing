1913eb00f4af9c3006f0b57630147a6ccff552d1
anayoA <mark.agwunobi@gmail.com>
2024-11-12 15:23:01 +0000

    Create tictactoe.java

---
+++ tictactoe/src/tictactoe.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class tictactoe {
+    public static void main(String[] args) {
+//        JFrame frame = new JFrame("TicTacToe");
+//        frame.setSize(720, 720);
+//        frame.setVisible(true);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setResizable(false);
+//
+//        frame.getContentPane().setBackground(new Color(159, 206, 223));
+    }
+
+
+}
